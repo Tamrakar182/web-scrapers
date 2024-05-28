a7981a004edf2ebdbef00fbe1b6e30436befdbc3
Manjul Tamrakar <manjultamrakar4@gmail.com>
2024-05-28 16:04:53 +0545

    fixed sharesansar scraping

--- sharesansar/README.md
+++ sharesansar/README.md
@@ -2,10 +2,17 @@
 A simple web scraper written using Beautiful Soup  and Selenium  that scrapes  stock price between given two dates from sharesansar.com
 
 The script will generate a csv file for each day and skip if there's no record
-# Requirements
-    pip install beautifulsoup4
-    pip install selenium
 
+# Setup 
+
+1. Setup virtual environment
+```python -m venv venv```
+
+2. Activate venv
+``` .\venv\Scripts\activate```
+
+3. Install requirements 
+```pip install -r requirements.txt.```
 
 # Usage
 1. Inside driver.py enter staring and ending date
--- sharesansar/driver.py
+++ sharesansar/driver.py
@@ -1,38 +1,46 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
-from datetime import date
-from selenium.webdriver.common.keys import Keys
+from datetime import date, datetime
 from scrape_table_all import scrape_table
 from return_dates import return_dates
 
-#Open the link
 browser = webdriver.Chrome()
 browser.maximize_window()
 browser.get("https://www.sharesansar.com/today-share-price")
-#Select the type of data to scrape
 
-#Select Commercial Bank
-searchBar=browser.find_element_by_id('sector')
-searchBar.send_keys('Commercial Bank')
+wait = WebDriverWait(browser, 10)
+select2_dropdown = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.select2-selection--single')))
+select2_dropdown.click()
 
-sdate = date(YYYY, MM, DD)
-edate = date(YYYY, MM, DD)
-dates = return_dates(sdate,edate)
+# enter sector type here
+sectorType = "Commercial Bank"
 
+option = wait.until(EC.element_to_be_clickable((By.XPATH, f'//li[contains(text(), "{sectorType}")]')))
+option.click()
+
+def set_date_input(date_input_id, day_str):
+    day = datetime.strptime(day_str, '%Y-%m-%d')
+    date_box = wait.until(EC.element_to_be_clickable((By.ID, date_input_id)))
+    date_box.clear()
+    date_box.send_keys(day.strftime('%Y-%m-%d'))
+
+# start and end date here (YYYY,MM,DD)
+sdate = date(2020, 1, 1)
+edate = date(2024, 5, 28)
+dates = return_dates(sdate, edate)
 
 for day in dates:
-    #Enter the date
-    date_box = browser.find_elements_by_id('fromdate')
-    date_box[0].clear()
-    date_box[0].send_keys(day)
-    #Click Search
-    searchBar=browser.find_element_by_id('btn_todayshareprice_submit')
-    searchBar.click()
-    time.sleep(3) #Needed for this sites
-    searchBar.send_keys(Keys.ENTER)
-    time.sleep(8) #Wait for data to show up longer wait time ensures data has loaded before scraping begins
-    #Scrape the table
+    set_date_input('fromdate', day)
+    
+    search_button = wait.until(EC.element_to_be_clickable((By.ID, 'btn_todayshareprice_submit')))
+    search_button.click()
+
+    time.sleep(8)
+
     html = browser.page_source
-    scrape_table(data=html,date=day)
+    scrape_table(data=html, date=day)
 
 browser.close()
